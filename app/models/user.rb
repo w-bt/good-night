b7e700dda9250eb612bbeb1f33ab8c875f303da8
Widhian Bramantya <widhian.bramantya@mekari.com>
2025-02-16 13:50:41 +0700

    feat(models/user.rb): update relationship with follows table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true
+
+  has_many :follower_relationships, foreign_key: :followee_id, class_name: "Follow", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :followee_relationships, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
+  has_many :followees, through: :followee_relationships, source: :followee
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
+
+  it { should have_many(:follower_relationships).with_foreign_key('followee_id').class_name('Follow').dependent(:destroy) }
+  it { should have_many(:followers).through(:follower_relationships).source(:follower) }
+
+  it { should have_many(:followee_relationships).with_foreign_key('follower_id').class_name('Follow').dependent(:destroy) }
+  it { should have_many(:followees).through(:followee_relationships).source(:followee) }
+
+  describe 'follower and followee relationships' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+
+    it 'allows a user to follow another user' do
+      user1.followees << user2
+      expect(user1.followees).to include(user2)
+      expect(user2.followers).to include(user1)
+    end
+
+    it 'allows a user to have followers' do
+      user2.followers << user1
+      expect(user2.followers).to include(user1)
+      expect(user1.followees).to include(user2)
+    end
+  end
 end
