3a3ce35be2720a220f31d4e49236758c533452fa
Widhian Bramantya <widhian.bramantya@mekari.com>
2025-02-16 12:59:02 +0700

    feat(user_controller.rb): add controller for user show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::UsersController < ApplicationController
     @users = UserService.new.all_users
     render json: @users, status: :ok
   end
+
+  def show
+    render json: @user, status: :ok
+  end
+
+  private
+
+  def set_user
+    @user = UserService.new.find_user(params[:id])
+  end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,10 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
   let(:user) { create(:user) }
+  let(:user_service) { instance_double(UserService) }
+
+  before do
+    allow(UserService).to receive(:new).and_return(user_service)
+    allow(user_service).to receive(:find_user).with(user.id.to_s).and_return(user)
+  end
 
   describe 'GET #index' do
+    before do
+      allow(user_service).to receive(:all_users).and_return([ user ])
+    end
+
     it 'returns a success response' do
       get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      allow(user_service).to receive(:find_user).with(user.id.to_s).and_return(user)
+    end
+
+    it 'returns a success response' do
+      get :show, params: { id: user.to_param }
       expect(response).to be_successful
     end
   end
