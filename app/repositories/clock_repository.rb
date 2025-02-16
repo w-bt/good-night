105a29fdb8762bea3e371055a95fbbf59e397c6d
Widhian Bramantya <widhian.bramantya@mekari.com>
2025-02-16 15:16:54 +0700

    feat(clock_repository.rb): add method to create clock

--- app/repositories/clock_repository.rb
+++ app/repositories/clock_repository.rb
@@ -6,4 +6,8 @@ class ClockRepository
   def find_active_clock
     @user.clocks.find_by(clock_out: nil)
   end
+
+  def create_clock(clock_in: nil, clock_out: nil)
+    @user.clocks.create(clock_in: clock_in, clock_out: clock_out)
+  end
 end
--- spec/repositories/clock_repository_spec.rb
+++ spec/repositories/clock_repository_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe ClockRepository, type: :repository do
       expect(clock).to be_nil
     end
   end
+
+  describe '#create_clock' do
+    it 'creates a new clock record' do
+      clock = repository.create_clock(clock_in: Time.current, clock_out: nil)
+      expect(clock).to be_persisted
+      expect(clock.user).to eq(user)
+      expect(clock.clock_in).not_to be_nil
+      expect(clock.clock_out).to be_nil
+    end
+  end
 end
